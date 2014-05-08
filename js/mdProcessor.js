4f44816298de7862eda23ed82508e4d7ffa0da34
Grant Gayed <grant_gayed@ca.ibm.com>
2014-05-08 15:01:43 -0400

    move mdProcessor.js, update License, remove marked.js

--- js/mdProcessor.js
+++ js/mdProcessor.js
@@ -2,24 +2,27 @@ var fs = require('fs');
 var path = require('path');
 var marked = require('marked');
 
-var SWITCH_SOURCEDIR = "--sourceDir", SWITCH_DESTINATIONDIR = "--destinationDir", SWITCH_OVERWRITE = "-overwrite";
-var EXTENSION_MARKDOWN = /\.md$/;
-var WRITE_PERMISSIONS = 0644;
+var EXTENSION_HTML = ".html";
+var EXTENSION_MARKDOWN_REGEX = /\.md$/g;
+var OPTIONS_MARKED = {sanitize: true, tables: true};
+var SWITCH_SOURCEDIR = "--sourceDir", SWITCH_DESTDIR = "--destDir", SWITCH_BASEURL = "--baseURL", SWITCH_OVERWRITE = "-overwrite";
 
-var sourceDir, destinationDir, overwrite;
+var sourceDir, destinationDir, baseURL, overwrite;
 
 process.argv.forEach(function(arg) {
 	if (arg.indexOf(SWITCH_SOURCEDIR) === 0 && arg.indexOf("=") !== -1) {
 		sourceDir = arg.substring(arg.indexOf("=") + 1);
-	} else if (arg.indexOf(SWITCH_DESTINATIONDIR) === 0 && arg.indexOf("=") !== -1) {
+	} else if (arg.indexOf(SWITCH_DESTDIR) === 0 && arg.indexOf("=") !== -1) {
 		destinationDir = arg.substring(arg.indexOf("=") + 1);
+	} else if (arg.indexOf(SWITCH_BASEURL) === 0 && arg.indexOf("=") !== -1) {
+		baseURL = arg.substring(arg.indexOf("=") + 1);
 	} else if (arg.indexOf(SWITCH_OVERWRITE) === 0) {
 		overwrite = true;
 	}
 });
 
 if (!sourceDir || !destinationDir) {
-	console.log("Usage: node mdProcessor " + SWITCH_SOURCEDIR + "=<sourceDirectory> " + SWITCH_DESTINATIONDIR + "=<destinationDirectory> [" + SWITCH_OVERWRITE + "]");
+	console.log("Usage: node mdProcessor " + SWITCH_SOURCEDIR + "=<sourceDirectory> " + SWITCH_DESTDIR + "=<destinationDirectory> [" + SWITCH_BASEURL + "=<baseURL>][" + SWITCH_OVERWRITE + "]");
 	process.exit();
 }
 
@@ -29,19 +32,29 @@ if (!fs.existsSync(sourceDir)) {
 }
 
 if (!fs.existsSync(destinationDir)) {
-	console.log("Destination directory does not exist: " + destinationDir);
-	process.exit();	
+	var result = fs.mkdirSync(destinationDir);
+	if (result) {
+		console.log("Failed to create destination directory: " + destinationDir);
+		console.log(result.toString());
+		process.exit();	
+	}
 }
 
 var writeStat = fs.statSync(destinationDir);
 var writeBlockSize = writeStat.blksize;
+
+if (baseURL) {
+	marked.InlineLexer.prototype.outputLink = baseURL;
+}
 				
 var filenames = fs.readdirSync(sourceDir);
 filenames.forEach(function(current) {
-	if (EXTENSION_MARKDOWN.test(current)) {
+	if (EXTENSION_MARKDOWN_REGEX.test(current)) {
 		var sourcePath = path.join(sourceDir, current);
-		fs.open(sourcePath, "r", null, function(err, readFd) {
-			if (readFd) {
+		fs.open(sourcePath, "r", null, function(readErr, readFd) {
+			if (readErr) {
+				console.log("Failed to open file to read: " + sourcePath + "\n" + readErr.toString());
+			} else {
 				var readStat = fs.fstatSync(readFd);
 				var readBlockSize = readStat.blksize;
 				var fileSize = readStat.size;
@@ -58,18 +71,18 @@ filenames.forEach(function(current) {
 				if (totalReadCount !== fileSize) {
 					console.log("Failed to read the full content of file " + sourcePath);
 				} else {
-					var fileText = inBuffer.toString("utf8", 0, inBuffer.length); // TODO use marked to generate HTML
-					var markdownText = marked(fileText, {
-						sanitize: true
-					});
+					var fileText = inBuffer.toString("utf8", 0, inBuffer.length);
+					var markdownText = marked(fileText, OPTIONS_MARKED);
 					if (!markdownText) {
 						console.log("Failed during conversion of markdown to HTML file " + sourcePath);
 					}
 					var outBuffer = new Buffer(markdownText);
-					var destinationPath = path.join(destinationDir, current);
-					var writeFlags = overwrite ? "w" : "wx";
-					fs.open(destinationPath, writeFlags, WRITE_PERMISSIONS, function(writeErr, writeFd) {
-						if (writeFd) {
+					var outputFilename = current.replace(EXTENSION_MARKDOWN_REGEX, EXTENSION_HTML);
+					var destinationPath = path.join(destinationDir, outputFilename);
+					fs.open(destinationPath, overwrite ? "w" : "wx", null, function(writeErr, writeFd) {
+						if (writeErr) {
+							console.log("Failed to open file to write: " + sourcePath + "\n" + writeErr.toString());
+						} else {
 							var totalWriteCount = 0;
 							do {
 								length = Math.min(writeBlockSize, outBuffer.length - totalWriteCount);
